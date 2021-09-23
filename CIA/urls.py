ffbd8dcf1818bc86e30289a2eb7763f25d541978
isdiscodead <isdiscodead@korea.ac.kr>
2021-09-24 00:49:48 +0900

    first commit test

--- CIA/urls.py
+++ CIA/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+import mainapp.urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mainapp.urls.MainListView.as_view(), name='main'),
     path('main/', include('mainapp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
